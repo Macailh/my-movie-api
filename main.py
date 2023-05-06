06ae3ee8a8567772189084607b9eebe2b80b6b41
Salvador German <salvadorenriquegb@gmail.com>
2023-05-06 08:29:34 -0600

    Add HTML reponse and movie list

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+movies_list = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'
+    },
+    {
+        'id': 2,
+        'title': 'Titanic',
+        'overview': "En el viaje inaugural del Titanic, el barco más grande jamás construido, Rose...",
+        'year': '1997',
+        'rating': 7.8,
+        'category': 'Drama, Romance'
+    },
+    {
+        'id': 3,
+        'title': 'Star Wars: Episodio IV - Una nueva esperanza',
+        'overview': "La princesa Leia es capturada y retenida como rehén por el malvado Imperio Galáctico ...",
+        'year': '1977',
+        'rating': 8.6,
+        'category': 'Acción, Aventura, Fantasía'
+    },
+    {
+        'id': 4,
+        'title': 'El señor de los anillos: La comunidad del anillo',
+        'overview': "La tranquilidad de la Comarca, una región del mundo de la Tierra Media habitada por ...",
+        'year': '2001',
+        'rating': 8.8,
+        'category': 'Acción, Aventura, Drama'
+    }
+]
+
+@app.get("/", tags=["home"])
 def root():
-    return {"message": "Start"}
+    return HTMLResponse("<h1>Start</h1>")
+
+@app.get("/movies", tags=["movies"])
+def get_all_movies():
+    return movies_list
